dc24a9aabf4530e2953bf91a55827dff09012dd8
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-10-12 17:14:08 -0300

    Fiz o Scriptable que representará um evento

---
+++ Assets/Scripts/EventSystem/GameEventSO.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EventSystem {
+    [CreateAssetMenu(fileName = "Game_Event", menuName = "Scriptable Objects/Event")]
+    public class GameEventSO : ScriptableObject {
+        private HashSet<UnityEvent> _unityEventsSet;
+        
+        private void OnEnable() {
+            _unityEventsSet = new HashSet<UnityEvent>();
+        }
+
+        public void InvokeEvent() {
+            foreach (var unityEvent in _unityEventsSet) {
+                unityEvent?.Invoke();
+            }
+        }
+        
+        public void SubscribeUnityEvent(UnityEvent unityEvent) {
+            _unityEventsSet.Add(unityEvent);
+        }
+
+        public void UnsubscribeUnityEvent(UnityEvent unityEvent) {
+            _unityEventsSet.Remove(unityEvent);
+        }
+    }
+}
