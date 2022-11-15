2adda0fb3cc4b1bb427b728c34051975463a6108
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-11-15 16:13:36 -0300

    Ao bater numa parede, sua velocidade se inverte

--- Assets/Scripts/Enemies/Agressor/AgressorMovementController.cs
+++ Assets/Scripts/Enemies/Agressor/AgressorMovementController.cs
@@ -6,6 +6,7 @@ namespace Enemies.Agressor {
     [RequireComponent(typeof(CharacterMovementHandler))]
     public class AgressorMovementController : MonoBehaviour {
         [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _visionDistance;
         private BoxCollider2D _collider2D;
         private CharacterMovementHandler _movementHandler;
         private Vector2 _currentDirection = Vector2.right;
@@ -20,18 +21,34 @@ namespace Enemies.Agressor {
         private void Update() {
             _movementHandler.Move(_currentDirection, _currentSpeed);
 
-            if (!CheckIfTheresFloorAhead()) {
+            if (!CheckIfTheresFloorAhead() || CheckIfTheresAWallAhead()) {
                 _currentDirection *= -1;
             }
+        }
+
+        private bool CheckIfTheresAWallAhead() {
+            var hit = CheckAhead();
+            
+            if(hit)
+                return hit.transform.tag != "Player";
+            return false;
         }
 
         private bool CheckIfTheresFloorAhead() {
             return Physics2D.Raycast(transform.position + new Vector3((_currentDirection.x* (_collider2D.size.x / 2)),0,0), Vector2.down);
         }
+        
+        private RaycastHit2D CheckAhead() {
+            return Physics2D.Raycast(transform.position + new Vector3((_currentDirection.x* (_collider2D.size.x / 2)),0,0), _currentDirection, _visionDistance);
+        }
 
         private void OnDrawGizmos() {
-            if(Application.isPlaying)
-                Gizmos.DrawRay(transform.position + new Vector3((_currentDirection.x* (_collider2D.size.x / 2)),0,0), Vector2.down);
+            if(Application.isPlaying) {
+                Gizmos.DrawRay(transform.position + new Vector3((_currentDirection.x * (_collider2D.size.x / 2)), 0, 0),
+                    Vector2.down);
+                Gizmos.DrawRay(transform.position + new Vector3((_currentDirection.x * (_collider2D.size.x / 2)), 0, 0),
+                    _currentDirection*_visionDistance);
+            }
         }
     }
 }
