ee771e3db1749848905981b89faa04964a955e50
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-12-09 16:45:21 -0300

    Comecando elementos

---
+++ Assets/Scripts/ElementSystem/PrimeElementSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace ElementSystem {
+    [CreateAssetMenu(menuName = "PrimeElement", fileName = "new Prime Element")]
+    public class PrimeElementSO : ScriptableObject {
+        
+    }
+}
