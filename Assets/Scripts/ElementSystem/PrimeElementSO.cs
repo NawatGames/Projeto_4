5daef9773f8732289d374f7131462f8b574c3a8c
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-12-12 14:24:24 -0300

    Criei os SOs que representam os elementos secundarios

--- Assets/Scripts/ElementSystem/PrimeElementSO.cs
+++ Assets/Scripts/ElementSystem/PrimeElementSO.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 namespace ElementSystem {
     [CreateAssetMenu(menuName = "PrimeElement", fileName = "new Prime Element")]
     public class PrimeElementSO : ScriptableObject {
-        
+        //Aqui teremos os sprites de plataforma e os comportamentos que elas terao de acordo com o elemento
     }
 }
--- Assets/Scripts/ElementSystem/SecundaryElementSO.cs
+++ Assets/Scripts/ElementSystem/SecundaryElementSO.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 namespace ElementSystem {
     [CreateAssetMenu(fileName = "Secundary Element", menuName = "new Secondary Element")]
     public class SecundaryElementSO : ScriptableObject {
+        //Aqui teremos os sprites de plataforma e os comportamentos que elas terao de acordo com o elemento
         
+        public PrimeElementSO[] _primeElementsComposerArray;
     }
 }
