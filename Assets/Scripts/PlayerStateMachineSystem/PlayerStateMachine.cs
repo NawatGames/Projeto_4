7dfbdc1e3e104f900a0429b7798638b0198af50c
Felipe Rodrigues <frps2003@gmail.com>
2022-12-16 19:33:01 -0300

    state machine created

---
+++ Assets/Scripts/PlayerStateMachineSystem/PlayerStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerStateMachineSystem
+{
+    public class PlayerStateMachine : MonoBehaviour
+    {
+        private PlayerState defaultPlayerState;
+        private PlayerState currentPlayerState;
+        private PlayerState lastFramePlayerState;
+        private PlayerStateMachineEventHandler playerStateMachineEventHandler;
+        private void LateUpdate()
+        {
+            lastFramePlayerState = currentPlayerState;
+        }
+
+        private void Update()
+        {
+            currentPlayerState.Execute(this);
+        }
+
+        public void SetState(PlayerState playerState)
+        {
+            playerState ??= defaultPlayerState;
+            var previewsState = currentPlayerState;
+            currentPlayerState = playerState;
+            playerStateMachineEventHandler.InvokeEvents(currentPlayerState,previewsState);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStateMachineSystem/StateTransition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerStateMachineSystem
+{
+    [Serializable]
+    public struct StateTransition
+    {
+        public Condition condition;
+        [Tooltip("Null to not change state")]
+        public PlayerState successCase;
+        [Tooltip("Null to not change state")]
+        public PlayerState failCase;
+    }
+}
