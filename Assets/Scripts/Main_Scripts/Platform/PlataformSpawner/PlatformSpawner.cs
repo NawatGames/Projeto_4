f2e28cac0940f4f1960a96d9e520cc5889511d21
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-12-21 17:50:25 -0300

    plataformas estao rotacionando. ainda nao perfeitamente

--- Assets/Scripts/Main_Scripts/Platform/PlataformSpawner/PlatformSpawner.cs
+++ Assets/Scripts/Main_Scripts/Platform/PlataformSpawner/PlatformSpawner.cs
@@ -12,7 +12,7 @@ namespace Main_Scripts.PlataformSpawner {
         [SerializeField] private GameEventSO SpawnEvent;
 
         [Header("Designers, decidam isso")]
-        [SerializeField] private bool _cleanSelectionOnSpawn;
+        [SerializeField] private bool _cleanElementSelectionOnSpawn;
         private void OnEnable() {
             eventToListen.SubscribeUnityEvent(SpawnPlatform);
         }
@@ -26,14 +26,35 @@ namespace Main_Scripts.PlataformSpawner {
                 return;
            
             var originalVector = initialEndPointPair.Item2 - initialEndPointPair.Item1;
-            var platformPosition = initialEndPointPair.Item2 - Vector2.right * (originalVector.x/2);
+            var platformPosition = initialEndPointPair.Item2 - Vector2.right * 0.5f;
+         
+            var vectorAngle = NormalizeAngleToFirstQuarter(Vector2.Angle(originalVector, Vector2.right));
+
+            print($"Ponto inicial: x: {initialEndPointPair.Item1.x} y: {initialEndPointPair.Item1.y}");
+            print($"Ponto final: x: {initialEndPointPair.Item2.x} y: {initialEndPointPair.Item2.y}");
+            Instantiate(platformPrefab, platformPosition, Quaternion.AngleAxis(CaparOAngulo(vectorAngle), Vector3.forward)).GetComponent<PlatformElementHandler>().Inialize(elementSelectedSingleton.Value);
             
-            Instantiate(platformPrefab, platformPosition, Quaternion.identity).GetComponent<PlatformElementHandler>().Inialize(elementSelectedSingleton.Value);
-            
-            if(_cleanSelectionOnSpawn) {
+            if(_cleanElementSelectionOnSpawn) {
                 elementSelectedSingleton.Value = null;
                 SpawnEvent.InvokeEvent();
             }
         }
+
+        private float NormalizeAngleToFirstQuarter(float angle) {
+            print(angle);
+            if (angle <= 180) return angle;
+            return 180 - angle;
+        }
+
+        private float CaparOAngulo(float angle) {
+            if (angle < 22.5) return 0f;
+            if (angle >= 22.5 && angle <= 67.5) return 45f;
+            if (angle > 67.5 && angle <= 90) return 90f;
+            if (angle < 112.5 && angle >= 90) return 90f;
+            if (angle >= 112.5 && angle <= 157.5) return 135f;
+             
+            return 180;
+        }
+        
     }
 }
