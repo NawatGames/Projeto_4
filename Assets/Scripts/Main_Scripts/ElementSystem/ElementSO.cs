1d8228f30bf60fd23d035cca377b76ca59379606
raphazik4 <rapha.marchetti@gmail.com>
2023-02-06 17:32:24 -0300

    Added sprites array to elementSO

--- Assets/Scripts/Main_Scripts/ElementSystem/ElementSO.cs
+++ Assets/Scripts/Main_Scripts/ElementSystem/ElementSO.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Main_Scripts.ElementSystem {
     public class ElementSO : ScriptableObject {
         public BaseBehaviourSO[] PlatformBehavioursArray;
-        public Sprite platformSprite;
+        public Sprite[] platformSprites;
         
         public Sprite bulletSprite;
         public ElementSO elementToLoseFor;
