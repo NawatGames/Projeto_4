5c560cde7290541e0004e47c1393320149878fd9
raphazik4 <rapha.marchetti@gmail.com>
2023-02-07 18:32:16 -0300

    Added index to identify which platform is whitch element

--- Assets/Scripts/Main_Scripts/ElementSystem/AllPrimeElementsContainer.cs
+++ Assets/Scripts/Main_Scripts/ElementSystem/AllPrimeElementsContainer.cs
@@ -3,5 +3,14 @@ using UnityEngine;
 namespace ElementSystem {
     public class AllPrimeElementsContainer : MonoBehaviour {
         public PrimeElementSO[] PrimeElementsArray;
+
+        private void Start() {
+            int index = 0;
+            foreach (var element in PrimeElementsArray)
+            {
+                element.elementSpawnIndex = index;
+                index++;
+            }
+        }
     }
 }
