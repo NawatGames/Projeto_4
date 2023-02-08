ee3d6e983f3b674a91e98d11f23bffb56446e159
Tagly5 <leotagliati56@gmail.com>
2023-02-08 17:16:41 -0300

    Sistema Menu Inicial Finalizado

---
+++ Assets/Scripts/QuitButtonHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class QuitButtonHandler : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("quitaaaando");
+        Application.Quit();
+    }
+}
