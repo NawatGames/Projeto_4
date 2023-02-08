4e41448b5b6ad64c59028b1b9e87fef800716583
raphazik4 <rapha.marchetti@gmail.com>
2023-02-08 18:33:12 -0300

    Fixed bullets rotation

--- Assets/Scripts/Main_Scripts/Enemies/Shooter/ShotPrefabs.cs
+++ Assets/Scripts/Main_Scripts/Enemies/Shooter/ShotPrefabs.cs
@@ -9,11 +9,21 @@ namespace Main_Scripts.Enemies.Shooter {
         [SerializeField] private GameObject _prefabToShoot;
         [SerializeField] private ElementSO bulletElementToShot;
         [SerializeField] private NoTypeGameEvent bulletDestroyedEvent;
+
+        private GameObject _instantiatedBullet;
         public void ShotPrefab(Vector2 direction) {
-            var gameObject = Instantiate(_prefabToShoot, transform.position, Quaternion.identity);
-            gameObject.AddComponent<BulletMoverMonoBehaviour>().Initialize(direction, 3f);
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-            gameObject.AddComponent<BulletElementHandler>().InitializeBullet(bulletElementToShot, bulletDestroyedEvent);
+            var angle = NormalizeAngle(direction);
+            if (direction.y >= 0) _instantiatedBullet = Instantiate(_prefabToShoot, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+            else _instantiatedBullet = Instantiate(_prefabToShoot, transform.position, Quaternion.AngleAxis(angle + 180, Vector3.forward));
+            _instantiatedBullet.AddComponent<BulletMoverMonoBehaviour>().Initialize(direction, 6f);
+            _instantiatedBullet.GetComponent<Rigidbody2D>().gravityScale = 0;
+            _instantiatedBullet.AddComponent<BulletElementHandler>().InitializeBullet(bulletElementToShot, bulletDestroyedEvent);
+        }
+
+        private float NormalizeAngle(Vector2 vector) {
+            if (vector.y > 0) 
+                return Vector2.Angle(vector, Vector2.right);            
+            return 180 - Vector2.Angle(vector, Vector2.right);
         }
     }
 }
