8bbe523dd76c1bf63415a939e6886417e48e85b5
raphazik4 <rapha.marchetti@gmail.com>
2023-02-09 15:25:20 -0300

    Updating scripts to solve agressor visibility

--- Assets/Scripts/Main_Scripts/Enemies/Agressor/AgressorMovementController.cs
+++ Assets/Scripts/Main_Scripts/Enemies/Agressor/AgressorMovementController.cs
@@ -13,15 +13,25 @@ namespace Enemies.Agressor {
         private Vector2 _currentDirection = Vector2.right;
         private float _currentSpeed;
 
+        private string _playerTag = "Player";
+        private Vector3 _eyePosition;
+        private Vector3 _floorScanPosition;
+
         private void Awake() {
             _movementHandler = GetComponent<CharacterMovementHandler>();
             _currentSpeed = _speedPatrolMode;
+
+            // Calculating positions to use in raycasting
+            var collliderSizeInX = _collider2D.size.x * gameObject.transform.localScale.x / 2;
+            var collliderSizeInY = _collider2D.size.y * gameObject.transform.localScale.y / 2;
+            _eyePosition = transform.position + new Vector3( ( _currentDirection.x + collliderSizeInX ), 0f, 0f);
+            _floorScanPosition = transform.position + new Vector3(collliderSizeInX, -collliderSizeInY, 0f);
         }
 
         private void Update() {
             _movementHandler.Move(_currentDirection, _currentSpeed);
 
-            if (!CheckIfTheresFloorAhead() || CheckIfTheresAWallAhead()) {
+            if (!CheckDown() || CheckIfTheresAWallAhead()) {
                 _currentDirection *= -1;
             }
 
@@ -38,7 +48,7 @@ namespace Enemies.Agressor {
             var hit = CheckAhead();
             
             if(hit)
-                return hit.transform.CompareTag("Player");
+                return hit.transform.CompareTag(_playerTag);
             return false;
         }
         
@@ -46,24 +56,26 @@ namespace Enemies.Agressor {
             var hit = CheckAhead();
             var hitDistance = hit?Vector2.Distance(transform.position, hit.transform.position):Mathf.Infinity;
             if(hit)
-                return !hit.transform.CompareTag("Player") && hitDistance <= _collider2D.size.x+0.5f;
+                return !hit.transform.CompareTag(_playerTag) && hitDistance <= _collider2D.size.x+0.5f;
             return false;
         }
 
-        private bool CheckIfTheresFloorAhead() {
-            return Physics2D.Raycast(transform.position + new Vector3((_currentDirection.x* (_collider2D.size.x / 2)),0,0), Vector2.down, 1f);
+        private bool CheckDown() {
+            var rayCast = Physics2D.Raycast(_floorScanPosition, Vector2.down, 1f);
+            Debug.Log("Down view: " + rayCast.collider.gameObject.name);
+            return rayCast;
         }
         
         private RaycastHit2D CheckAhead() {
-            return Physics2D.Raycast(transform.position + new Vector3((_currentDirection.x* (_collider2D.size.x / 2)),0,0), _currentDirection, _visionDistance);
+            var rayCast = Physics2D.Raycast(_eyePosition, _currentDirection, _visionDistance);
+            Debug.Log("Front view: " + rayCast.collider.gameObject.name);
+            return rayCast;
         }
 
         private void OnDrawGizmos() {
             if(Application.isPlaying) {
-                Gizmos.DrawRay(transform.position + new Vector3((_currentDirection.x * (_collider2D.size.x / 2)), 0, 0),
-                    Vector2.down);
-                Gizmos.DrawRay(transform.position + new Vector3((_currentDirection.x * (_collider2D.size.x / 2)), 0, 0),
-                    _currentDirection*_visionDistance);
+                Gizmos.DrawRay(_floorScanPosition, Vector2.down);
+                Gizmos.DrawRay(_eyePosition, _currentDirection * _visionDistance);
             }
         }
     }
