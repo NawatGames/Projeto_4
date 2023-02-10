f55d8a13e81fedaad6f0ffa45b5c1ae94fe35620
raphazik4 <rapha.marchetti@gmail.com>
2023-02-09 21:00:09 -0300

    Build reflections

--- Assets/Scripts/Main_Scripts/Enemies/Shooter/BulletElementHandler.cs
+++ Assets/Scripts/Main_Scripts/Enemies/Shooter/BulletElementHandler.cs
@@ -2,6 +2,8 @@ using Main_Scripts.ElementSystem;
 using Main_Scripts.EventSystem.SimpleEvents;
 using Main_Scripts.Platform;
 using System.Collections;
+using Main_Scripts.SingletonsSO;
+using Enemies.Shooter;
 using UnityEngine;
 
 namespace Main_Scripts.Enemies.Shooter {
@@ -9,25 +11,57 @@ namespace Main_Scripts.Enemies.Shooter {
     public class BulletElementHandler : MonoBehaviour {
         private ElementSO _bulletElement;
         private NoTypeGameEvent eventToTrigger;
-        
+
+        // Yes, fix later
+        private BulletMoverMonoBehaviour _selfMover;
+        public ElementSO fire_element;
+        public ElementSO glass_element;
+        public ElementSO ground_element;
+        public ElementSO water_element;
+        public ElementSO wind_element;
+        public ElementSelectedSingleton elementSelectedSingleton;
+
         public void InitializeBullet(ElementSO elementSo, NoTypeGameEvent eventBulletDestroyed) {
             GetComponent<Collider2D>().isTrigger = true;
             _bulletElement = elementSo;
             GetComponent<SpriteRenderer>().sprite = _bulletElement.bulletSprite;
             eventToTrigger = eventBulletDestroyed;
+            _selfMover = GetComponent<BulletMoverMonoBehaviour>();
             StartCoroutine(StartProjectileDestroyTimer(5f));
+        }
+
+        public void FillBulllet(
+            ElementSO f_fire_element,
+            ElementSO f_glass_element,
+            ElementSO f_ground_element,
+            ElementSO f_water_element,
+            ElementSO f_wind_element,
+            ElementSelectedSingleton f_elementSelectedSingleton
+        ){
+            fire_element = f_fire_element;
+            glass_element = f_glass_element;
+            ground_element = f_ground_element;
+            water_element = f_water_element;
+            wind_element = f_wind_element;
+            elementSelectedSingleton = f_elementSelectedSingleton;
         }
 
         public void ChangeElement(ElementSO elementSo) {
             _bulletElement = elementSo;
             GetComponent<SpriteRenderer>().sprite = _bulletElement.bulletSprite;
         }
+
         private void OnTriggerEnter2D(Collider2D col) {
             if (col.CompareTag("Platform")) {
                 var platformElement = col.gameObject.GetComponent<PlatformElementHandler>().PlatformElement;
+                Debug.Log("Fire: " + fire_element);
                 if (platformElement == _bulletElement.elementToLoseFor) {
                     Destroy(gameObject);
                     eventToTrigger?.InvokeEvent();
+                }
+                if (platformElement == glass_element){
+                    _selfMover.Reflect(col.gameObject);
+                    ChangeElement(elementSelectedSingleton.Value);
                 }
             }
         }
--- Assets/Scripts/Main_Scripts/Enemies/Shooter/BulletMoverMonoBehaviour.cs
+++ Assets/Scripts/Main_Scripts/Enemies/Shooter/BulletMoverMonoBehaviour.cs
@@ -16,5 +16,22 @@ namespace Enemies.Shooter {
             _speedDiretion = speedDirection;
             _rgb.velocity = _speedDiretion * _speed;
         }
+
+        public void Reflect(GameObject platform){
+            // Reflect
+            _speedDiretion = Vector2.Reflect(_speedDiretion, platform.transform.up);
+            // Rotate
+            var angle = NormalizeAngle(_speedDiretion);
+            // Apply
+            if (_speedDiretion.y >= 0) _rgb.gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            else _rgb.gameObject.transform.rotation = Quaternion.AngleAxis(angle + 180, Vector3.forward);
+            _rgb.velocity = _speedDiretion * _speed;
+        }
+
+        private float NormalizeAngle(Vector2 vector) {
+            if (vector.y > 0) 
+                return Vector2.Angle(vector, Vector2.right);            
+            return 180 - Vector2.Angle(vector, Vector2.right);
+        }
     }
 }
